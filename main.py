4597c2fc04279011b3151c4871fe4fcb9ff0a046
Dima <asasasa_sassas@gmail.ru>
2024-08-05 18:12:45 +0300

    1 урок python

---
+++ main.py
@@ -0,0 +1,17 @@
+print("Hello world")
+print("Korobkov")
+print("Мой возраст 15 лет")
+print("Hello world")
+print((2 + 2 / 4) * 4)
+print(((3+2)*4)/((2**2)+3/4))
+print((3-(-3**2))/(9 * 3) * 4)
+a = "Мой возраст: "
+b = 25
+c = " лет."
+print(a + b + c)
+a = "10"
+b = 20
+print(a + b)
+a = "28"
+b = 24
+print(a + b)
